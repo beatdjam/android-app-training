893b9170fcfe87e2746428dea34715854870c488
beatdjam <beat3526@gmail.com>
2019-01-01 03:21:08 +0900

    Add a back button
    Add a back button in NextActivity.

--- app/src/main/java/com/example/myapplication/NextActivity.kt
+++ app/src/main/java/com/example/myapplication/NextActivity.kt
@@ -2,6 +2,7 @@ package com.example.myapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_next.*
 
 class NextActivity : AppCompatActivity() {
@@ -10,5 +11,15 @@ class NextActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_next)
         textView2.text = intent.getStringExtra("INPUT_DATA")
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            android.R.id.home -> finish()
+            else -> return super.onOptionsItemSelected(item)
+        }
+        return true
     }
 }
