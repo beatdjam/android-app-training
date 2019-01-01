f450b77ec9268051fea3f5f6dcf261c1a1820f1a
beatdjam <beat3526@gmail.com>
2019-01-01 14:48:05 +0900

    Add toast in NextActvity

--- app/src/main/java/com/example/myapplication/NextActivity.kt
+++ app/src/main/java/com/example/myapplication/NextActivity.kt
@@ -3,6 +3,7 @@ package com.example.myapplication
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_next.*
 
 class NextActivity : AppCompatActivity() {
@@ -11,8 +12,8 @@ class NextActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_next)
         textView2.text = intent.getStringExtra("INPUT_DATA")
-
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        Toast.makeText(this, "This page is next Activity", Toast.LENGTH_SHORT).show()
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
